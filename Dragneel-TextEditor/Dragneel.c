11abc13fc8aec275425db0dba6c0e8ce0ee2a7db
BlackPhoenix1747 <craftingcaptain456@gmail.com>
2024-02-23 21:32:24 +0500

    Simple Text Output Established

--- Dragneel-TextEditor/Dragneel.c
+++ Dragneel-TextEditor/Dragneel.c
@@ -6,22 +6,36 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <string.h>
 
 /* DEFINES */
 #define DRAG_VERSION "0.0.1"
 enum editorKey {
-  ARROW_LEFT = 'a',
-  ARROW_RIGHT = 'd',
-  ARROW_UP = 'w',
-  ARROW_DOWN = 's'
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY,
+  DEL_KEY
 };
 
 /* DATA */
+
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow row;
   struct termios orig_termios;
 };
 
@@ -57,7 +71,7 @@ void enableRawMode() {
 }
 
 /* Keypresses */
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -68,11 +82,33 @@ char editorReadKey() {
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if (seq[0] == '[') {
+       if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
       switch (seq[1]) {
-        case 'A': return 'j';
-        case 'B': return 'k';
-        case 'C': return 'l';
-        case 'D': return 'h';
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
       }
     }
     return '\x1b';
@@ -115,6 +151,19 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/* File I/O */
+void editorOpen() {
+  char *line = "Hello, World!";
+  ssize_t linelen = 13;
+
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
+}
+
+
 /*** append buffer ***/
 
 struct abuf {
@@ -136,26 +185,34 @@ void abFree(struct abuf *ab) {
 }
 
 /* Input */
-void editorMoveCursor() {
+void editorMoveCursor(int key) {
   switch (key) {
-    case 'h':
-      E.cx--;
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      }
       break;
-    case 'l':
-      E.cx++;
+    case ARROW_RIGHT:
+      if (E.cx != E.screencols - 1) {
+        E.cx++;
+      }
       break;
-    case 'j':
-      E.cy--;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
       break;
-    case 'k':
-      E.cy++;
+    case ARROW_DOWN:
+      if (E.cy != E.screenrows - 1) {
+        E.cy++;
+      }
       break;  
   }
 }
 
 
 void editorProcessKeyboard() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
     case 27:
@@ -164,11 +221,25 @@ void editorProcessKeyboard() {
       exit(0);
       break;
 
-
-    case 'j':
-    case 'k':
-    case 'h':
-    case 'l':
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+      
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
   }
@@ -178,20 +249,27 @@ void editorProcessKeyboard() {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screencols / 3) {
-    char welcome[80]; 
-    int welcomelen = snprintf(welcome, sizeof(welcome), "Dragneel Editor -- version %s", DRAG_VERSION);
-    if (welcomelen > E.screencols) welcomelen = E.screencols;
-    int padding = (E.screencols - welcomelen) / 2;
-    if (padding) {
-      abAppend(ab, "~", 1);
-      padding--;
+    if (y >= E.numrows) {
+      if (y == E.screencols / 3) {
+        char welcome[80]; 
+        int welcomelen = snprintf(welcome, sizeof(welcome), "Dragneel Editor -- version %s", DRAG_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
+        abAppend(ab, "~", 1);
+      }
+    } else {
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row.chars, len);
     }
-    while (padding--) abAppend(ab, " ", 1);
-    abAppend(ab, welcome, welcomelen);
-  } else {
-    abAppend(ab, "~", 1);
-  }
+
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows -1) {
       abAppend(ab, "\r\n", 2);
@@ -222,8 +300,9 @@ void editorRefreshScreen() {
 /* Main Program */
 
 void mainEditor() {
-  E.cx =0;
-  E.cy =0;
+  E.cx = 0;
+  E.cy = 0;
+  E.numrows = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
@@ -231,6 +310,7 @@ void mainEditor() {
 int main() {
   enableRawMode();
   mainEditor();
+  editorOpen();
   
   while(1) {
     editorRefreshScreen();
